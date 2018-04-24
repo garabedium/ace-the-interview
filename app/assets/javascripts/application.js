e74b3f942f17fb5b7421eee948cbdf14abc6f29c
Greg Garabedian <ggarabedian09@gmail.com>
2018-04-24 19:00:33 -0400

    chore: Foundation upgrade and config

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,53 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+
+// Foundation Dependencies:
+// Add '= require' to include plugin
+///////////////////////////////////////
+//= require foundation.core.js
+// foundation.abide.js
+// foundation.accordion.js
+// foundation.accordionMenu.js
+// foundation.drilldown.js
+// foundation.dropdown.js
+// foundation.dropdownMenu.js
+// foundation.equalizer.js
+// foundation.interchange.js
+// foundation.magellan.js
+// foundation.offcanvas.js
+// foundation.orbit.js
+// foundation.responsiveMenu.js
+// foundation.responsiveToggle.js
+//= require foundation.reveal.js
+// foundation.slider.js
+// foundation.sticky.js
+// foundation.tabs.js
+// foundation.toggler.js
+// foundation.tooltip.js
+// foundation.util.box.js
+// foundation.util.keyboard.js
+// foundation.util.mediaQuery.js
+// foundation.util.motion.js
+// foundation.util.nest.js
+// foundation.util.timerAndImageLoader.js
+// foundation.util.touch.js
+// foundation.util.triggers.js
+// foundation.zf.responsiveAccordionTabs.js'
+///////////////////////////////////////
+
+//= require activestorage
+//= require_tree .
+
+$(function(){ $(document).foundation(); });
