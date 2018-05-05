95662f6f58524118282714657fbdf212e8910db7
Greg Garabedian <ggarabedian09@gmail.com>
2018-05-05 19:29:13 -0400

    chore: create questions table, modified sign up form

--- db/migrate/20180503022815_create_questions.rb
+++ db/migrate/20180503022815_create_questions.rb
@@ -1,8 +1,9 @@
 class CreateQuestions < ActiveRecord::Migration[5.2]
   def change
     create_table :questions do |t|
-      t.string :title, null: false, unique: true
+      t.string :title, null: false
       t.timestamps
     end
+    add_index :questions, :title, unique: true
   end
 end
