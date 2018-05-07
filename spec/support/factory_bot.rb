c43ef00a63a032c9abb13b47899cc54660cb3ce7
Greg Garabedian <ggarabedian09@gmail.com>
2018-05-07 10:55:03 -0400

    refactor: factory bot include required user fields

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -3,7 +3,10 @@ require 'factory_bot'
 FactoryBot.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com" }
-
+    first_name 'Lorem'
+    last_name 'Ipsum'
+    zip '02918'
+    # admin { false }
     password 'password'
     password_confirmation 'password'
   end
