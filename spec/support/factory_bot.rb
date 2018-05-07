5dbd2dabeff9e585824e5a84e4be110b140267b0
Greg Garabedian <ggarabedian09@gmail.com>
2018-05-07 15:05:18 -0400

    refactor: update user_sign_up spec to include select field

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,8 +1,13 @@
 require 'factory_bot'
 
 FactoryBot.define do
+
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com" }
+    first_name 'Lorem'
+    last_name 'Ipsum'
+    industry { Industry.create!(name:'Some Industry') }
+    zip '02918'
     password 'password'
     password_confirmation 'password'
   end
