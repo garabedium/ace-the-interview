5995526cb86e6cd383c1b81262a09789c62ac29e
Greg Garabedian <ggarabedian09@gmail.com>
2018-05-10 19:44:05 -0400

    feat: redirect after login/sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,12 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # def after_sign_in_path_for(resource)
-  #   redirect_to users_path
-  # end
+  def after_sign_in_path_for(resource)
+    '/app'
+  end
+
+  def after_sign_up_path_for(resource)
+    '/app'
+  end
 
 end
