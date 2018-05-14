e811e687b83056035eb02724734639d9ddb0c100
Greg Garabedian <ggarabedian09@gmail.com>
2018-05-14 11:45:01 -0400

    feat: parse files by category type

--- app/seeders/test_seeder.rb
+++ app/seeders/test_seeder.rb
@@ -1,29 +1,43 @@
+require 'pry'
+
 class TestSeeder
 
-  QUESTIONS_CSS = []
+  QUESTIONS = {}
 
+  def self.parse_file(type)
+    QUESTIONS["#{type}"] = []
 
-  def self.parse_file!
-    File.open('data/questions-css.txt', 'r') do |file|
+    File.open("app/seeders/data/questions-#{type}.txt", 'r') do |file|
       file.each do |line|
-        line = line.strip
-        QUESTIONS_CSS.push(line)
+        line = line.squish
+        QUESTIONS["#{type}"].push(line)
       end
     end
   end
 
+  # def self.create_questions
+
+  # end
+
   def self.seed!
-    parse_file!
+    categories = ["CSS","HTML"]
 
-    QUESTIONS_CSS.each do |item|
-      question = Question.find_or_initialize_by(title: item)
-      question.user = User.first
-      question.public = "public"
-      # question = question.save!
-      question_category = QuestionCategory.find_or_initialize_by(question: question, category: Category.find_by(name: "CSS"))
-
-      question_category.save!
+    categories.each do |category|
+      parse_file("#{category.downcase}")
     end
+
+binding.pry
+    # parse_file("css")
+    # parse_file("html")
+
+    # QUESTIONS.each_with_index do |item,index|
+    #   question = Question.find_or_initialize_by(title: item)
+    #   question.user = User.first
+    #   question.public = "public"
+    #   # question_category = QuestionCategory.find_or_initialize_by(question: question, category: Category.find_by(name: "CSS"))
+    #   question_category = QuestionCategory.find_or_initialize_by(question: question, category: Category.find_by(name: categories["index"]))
+    #   question_category.save!
+    # end
 
   end
 
